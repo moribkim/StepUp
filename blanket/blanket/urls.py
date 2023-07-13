dec9187a310513dbbbb0469e5ac90c7838ec492c
jian <jiankimr@gmail.com>
2023-07-13 16:41:52 +0900

    account

--- blanket/blanket/urls.py
+++ blanket/blanket/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from account import views as account
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('login/',include('account.urls')),
+    path('',include('account.urls')),
 ]
