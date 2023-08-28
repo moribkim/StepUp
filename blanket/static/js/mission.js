c59bbc5c91a5ed30be90a45e129fcbd6b731d783
ChaCha-CSY <cha736774@gmail.com>
2023-08-28 18:29:01 +0900

    jquerydown

--- blanket/static/js/mission.js
+++ blanket/static/js/mission.js
@@ -1,15 +1,64 @@
 //
 // Page convertion animation
 //
-document.addEventListener('DOMContentLoaded', () => {
-    window.setTimeout(() => {
-        document.body.classList.remove('fade');
-    }, );
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     window.setTimeout(() => {
+//         document.body.classList.remove('fade');
+//     }, );
+// });
 
 //
 //Help
 //
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    let missionButtons = document.querySelectorAll('.main-mission, .sub-mission');
+    missionButtons.forEach(button => {
+
+        button.addEventListener('click', function(event) {
+            event.preventDefault();  // 기본 이벤트를 중지합니다.
+
+            let missionId = button.getAttribute('data-mission-id');
+
+            fetch(`/complete_mission_js/${missionId}/`, {
+                method: 'POST',
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-CSRFToken': getCookie('csrftoken')
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.completed) {
+                    // 미션을 완료한 경우의 UI 업데이트 (예: 텍스트 변경)
+                    missionComplete();
+                } else {
+                    // 미션을 완료하지 않은 경우의 UI 업데이트
+                    missionInComplete();
+                }
+            });
+        });
+
+    });
+});
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
 const HelpBtn = document.getElementsByClassName("Btn-help")[0];
 const Help = document.getElementsByClassName("Help")[0];
 const HelpClose = document.getElementsByClassName("HelpClose")[0];
@@ -49,52 +98,64 @@ modalClose.addEventListener('click', () => {
     windowBtn.style.pointerEvents = "auto";
 });
 
+// 미션 페이지가 새로고침 되었을 때
 document.addEventListener("DOMContentLoaded", function() {
     let mainMissionCompleted = document.querySelector('.main-mission').getAttribute('data-completed') === 'true';
-    let allSubMissions = document.querySelectorAll('.serve-mission');
-    let allSubMissionsCompleted = Array.from(allSubMissions).every(mission => mission.getAttribute('data-completed') === 'true');
+    let allSubMissions = document.querySelectorAll('.sub-mission');
 
-    if (mainMissionCompleted) {
-        // 미션이 완료된 경우 바로 표시
-        displayui();
+    modal.style.display = "flex";
+    if (mainMissionCompleted === false) {
+        missionInComplete();
+    }
+    else{
+        missionComplete();
     }
 });
 
-function displayui() {
-    // 완료되었을 때 즉각적으로 보이기 바라는 ui
-    page = "full";
-}
+
+// function displayui() {
+//     // 미완료되었을 때 즉각적으로 보이기 바라는 ui, 팝업창의 상태
+//     modal.style.transition = "top 1s cubic-bezier(0.2, .5, .5, 1)";
+//     modal.style.top = "844px";
+//     footer.style.opacity = 1;
+//     windowBtn.disabled = false;
+//     windowBtn.style.pointerEvents = "auto";
+// }
 
 //
 // Mission Popup page
 //
 
+const ReloadBtn = () => {
+    $(".mission-popup:button").load(location.href + " .mission-popup:button");
+}
+
 const mainBtn = document.querySelector('.main-mission');
 const misFixed = document.querySelector('.mission-fixed');
 const misExplain = document.querySelector('.mission-explanation');
 
-const serNum = 3;
-const serBtn = document.querySelectorAll('.serve-mission');
-const serMis = document.querySelectorAll('.serve-content');
-const serShuf = documnet.querySelectorAll('.serve-shuffle')
+const subNum = 3;
+const subBtn = document.querySelectorAll('.sub-mission');
+const subMis = document.querySelectorAll('.sub-content');
+const subShuf = document.querySelectorAll('.sub-shuffle');
 const record = document.querySelector('.Btn-record');
 
 //Main Mission complete
-//change main mission style, activate serve mission button
+//change main mission style, activate sub mission button
 let mainComplete = false;
 
 const missionComplete = () => {
-
+    ReloadBtn();
     mainBtn.style.backgroundColor = "#FFF7D9";
-    misFixed.innerHTML = "미션 완료!";
-    misExplain = "";
+    misFixed.innerHTML = "";
+    misExplain.innerHTML = "미션 완료!";
 
-    for(let i=0; i < serNum; i++){
-        serBtn[i].disabled = false;
-        serBtn[i].style.backgroundImage = "none";
-        serBtn[i].style.opacity = 0.9;
-        serMis[i].style.opacity = 1;
-        serShuf[i].style.display = 'block';
+    for(let i=0; i < subNum; i++){
+        subBtn[i].disabled = false;
+        subBtn[i].style.backgroundImage = "none";
+        subBtn[i].style.opacity = 0.9;
+        subMis[i].style.opacity = 1;
+        subShuf[i].style.display = 'block';
     };
     //record open
     record.disabled = false;
@@ -105,17 +166,18 @@ const missionComplete = () => {
 //Main Mission Incomplete
 //change main mission style, deactivate serve mission button
 const missionInComplete = () => {
+    ReloadBtn();
     mainBtn.style.backgroundColor = "#C6C993";
     misFixed.innerHTML = "오늘 하루의 첫 미션";
     misExplain.innerHTML = "베개와 이불을 제자리로 정돈하기";    
 
-    for(let i=0; i < serNum; i++){
-        serBtn[i].disabled = true;
-        serBtn[i].style.opacity = 0.5;
-        serMis[i].style.opacity = 0;
-        serShuf[i].style.display = 'none';
+    for(let i=0; i < subNum; i++){
+        subBtn[i].disabled = true;
+        subBtn[i].style.opacity = 0.5;
+        subMis[i].style.opacity = 0;
+        subShuf[i].style.display = 'none';
         setTimeout(()=>{
-            serBtn[i].style.backgroundImage = "url(../images/locked-image.png)";
+            subBtn[i].style.backgroundImage = "url(/static/locked-image.png)";
         }, 600);
     };
     //record open
@@ -124,21 +186,9 @@ const missionInComplete = () => {
     record.style.backgroundColor = "#C6C993";
 }
 
-//Main mission complete and change contents
-mainBtn.addEventListener('click', () => {
-    if(mainComplete === true){
-        mainComplete = false;
-        missionInComplete();
-    }
-    else if(mainComplete === false){
-        mainComplete = true;
-        missionComplete();
-    }
-});
-
-//Serve mission complete and change color
-for(let i=0; i < serNum; i++){
-    serBtn[i].addEventListener('click', () => {
-        serBtn[i].style.backgroundColor = "#FFF7D9";
+//Sub mission complete and change color
+for(let i=0; i < subNum; i++){
+    subBtn[i].addEventListener('click', () => {
+        subBtn[i].style.backgroundColor = "#FFF7D9";
     });
 };
